0eb465ba3942e7bf977bd83228ad0b7cf4c90900
alpen-wolf <67392785+alpen-wolf@users.noreply.github.com>
2023-01-12 16:05:31 +0000

    Added option to copy the visuals struct
    Added option to copy the visuals struct and with better formatting to previously

--- src/app/preview.rs
+++ src/app/preview.rs
@@ -6,61 +6,62 @@ use super::{Mode, Theme, simplified::Settings};
 
 pub fn preview(mode: &mut Mode, themes: &mut Vec<Theme>, selected_theme: &mut usize, ctx: & eframe::egui::Context) {
     eframe::egui::CentralPanel::default().show(ctx, |ui| {
-        let mut remove: (bool, usize) = (false, 0);
-        for (i, theme) in themes.iter().enumerate() {
-            if i == 0 || i == 1 {
-                let frame = eframe::egui::Frame::none().show(ui, |ui| {
-                    ui.horizontal(|ui| {
-                        ui.collapsing(format!("{} - {}", theme.name.clone(), i) ,|ui| {
-                            ui.label(theme.author.clone())
+        eframe::egui::scroll_area::ScrollArea::vertical().show(ui, |ui| {
+            let mut remove: (bool, usize) = (false, 0);
+            for (i, theme) in themes.iter().enumerate() {
+                if i == 0 || i == 1 {
+                    let frame = eframe::egui::Frame::none().show(ui, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.collapsing(format!("{} - {}", theme.name.clone(), i) ,|ui| {
+                                ui.label(theme.author.clone())
+                            });
                         });
                     });
-                });
-                if frame.response.interact(eframe::egui::Sense::click()).clicked() {
-                    *selected_theme = i;
-                };
-                if frame.response.interact(eframe::egui::Sense::click()).double_clicked() {
-                    *mode = Mode::Editor;
-                    *selected_theme = i;
-                };
-            }
-            else {
-                let frame = eframe::egui::Frame::none().show(ui, |ui| {
-                    ui. horizontal(|ui| {
-                        ui.collapsing(format!("{} - {}", theme.name.clone(), i) ,|ui| {
-                            ui.label(format!("Author: {}", theme.author));
-                            if ui.button("Remove").clicked() {
-                                
-                                remove = (true, i);
-                            };
-                        })
+                    if frame.response.interact(eframe::egui::Sense::click()).clicked() {
+                        *selected_theme = i;
+                    };
+                    if frame.response.interact(eframe::egui::Sense::click()).double_clicked() {
+                        *mode = Mode::Editor;
+                        *selected_theme = i;
+                    };
+                }
+                else {
+                    let frame = eframe::egui::Frame::none().show(ui, |ui| {
+                        ui. horizontal(|ui| {
+                            ui.collapsing(format!("{} - {}", theme.name.clone(), i) ,|ui| {
+                                ui.label(format!("Author: {}", theme.author));
+                                if ui.button("Remove").clicked() {
+                                    
+                                    remove = (true, i);
+                                };
+                            })
+                        });
                     });
-                });
-                if frame.response.interact(eframe::egui::Sense::click()).clicked() {
-                    *selected_theme = i;
-                };
-                if frame.response.interact(eframe::egui::Sense::click()).double_clicked() {
-                    *mode = Mode::Editor;
-                    *selected_theme = i;
-                };
+                    if frame.response.interact(eframe::egui::Sense::click()).clicked() {
+                        *selected_theme = i;
+                    };
+                    if frame.response.interact(eframe::egui::Sense::click()).double_clicked() {
+                        *mode = Mode::Editor;
+                        *selected_theme = i;
+                    };
+                }
             }
-        }
-        if ui.button("New Theme").clicked() {
-            themes.push(Theme::default())
-        }
-        if ui.button("Theme Wizard").clicked() {
-            *mode = Mode::Simplified(Settings::default());
-        }
-        
-        ui.collapsing("Visuals struct as text", |ui| {
-            let mut temp = &mut format!("{:?}", themes[selected_theme.clone()].visuals).clone();
-            ui.add(eframe::egui::TextEdit::multiline(temp).desired_width(1000.));
+            if ui.button("New Theme").clicked() {
+                themes.push(Theme::default())
+            }
+            if ui.button("Theme Wizard").clicked() {
+                *mode = Mode::Simplified(Settings::default());
+            }
+            
+            ui.collapsing("Visuals struct as text", |ui| {
+                ui.label(format!("{:#?}", themes[selected_theme.clone()].visuals));
+            });
+            
+            if remove.0 == true {
+                *selected_theme = remove.1 - 1;
+                themes.remove(remove.1);
+            }
         });
-        
-        if remove.0 == true {
-            *selected_theme = remove.1 - 1;
-            themes.remove(remove.1);
-        }
     });
 }
 
